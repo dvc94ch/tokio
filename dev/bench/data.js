d9e4b788a15a8faffeb6bdfa15b52729d0499d3b
github-action-benchmark <github@users.noreply.github.com>
2020-11-21 13:08:12 +0000

    add rt_multi_threaded (cargo) benchmark result for f927f01a34d7cedf0cdc820f729a7a6cd56e83dd

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1605964086961,
+  "lastUpdate": 1605964092314,
   "repoUrl": "https://github.com/dvc94ch/tokio",
   "entries": {
     "sync_rwlock": [
@@ -53,6 +53,56 @@ window.BENCHMARK_DATA = {
             "name": "read_uncontended",
             "value": 594,
             "range": "± 92",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ],
+    "rt_multi_threaded": [
+      {
+        "commit": {
+          "author": {
+            "email": "eliza@buoyant.io",
+            "name": "Eliza Weisman",
+            "username": "hawkw"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "f927f01a34d7cedf0cdc820f729a7a6cd56e83dd",
+          "message": "macros: fix rustfmt on 1.48.0 (#3160)\n\n## Motivation\r\n\r\nLooks like the Rust 1.48.0 version of `rustfmt` changed some formatting\r\nrules (fixed some bugs?), and some of the code in `tokio-macros` is no\r\nlonger correctly formatted. This is breaking CI.\r\n\r\n## Solution\r\n\r\nThis commit runs rustfmt on Rust 1.48.0. This fixes CI.\r\n\r\nCloses #3158",
+          "timestamp": "2020-11-20T10:19:26-08:00",
+          "tree_id": "bd0243a653ee49cfc50bf61b00a36cc0fce6a414",
+          "url": "https://github.com/dvc94ch/tokio/commit/f927f01a34d7cedf0cdc820f729a7a6cd56e83dd"
+        },
+        "date": 1605964088813,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "chained_spawn",
+            "value": 193077,
+            "range": "± 15586",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ping_pong",
+            "value": 728781,
+            "range": "± 43522",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spawn_many",
+            "value": 5447180,
+            "range": "± 773889",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "yield_many",
+            "value": 21078393,
+            "range": "± 2685501",
             "unit": "ns/iter"
           }
         ]
